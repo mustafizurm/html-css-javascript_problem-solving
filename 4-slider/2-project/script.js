cb4d9377f41d46e674d6127d16c5e59a9657fab2
Mustafizur <mustafizurrahman1256@gmail.com>
2024-09-07 22:45:25 +0600

    project push

--- 4-slider/2-project/script.js
+++ 4-slider/2-project/script.js
@@ -1,35 +1,95 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+// first
 
-menuBtn.addEventListener("click", ()=>{
-    menuBtn.classList.toggle("active");
-    navigation.classList.toggle("active")
-})
+// const menuBtn = document.querySelector(".menu-btn");
+// const navigation = document.querySelector(".navigation");
 
-const btns = document.querySelectorAll(".nav-btn");
-const slides = document.querySelectorAll(".img-slide");
-const contents = document.querySelectorAll(".content");
+// menuBtn.addEventListener("click", ()=>{
+//     menuBtn.classList.toggle("active");
+//     navigation.classList.toggle("active")
+// })
 
-var sliderNav = function (manuaL){
-    btns.forEach((btn)=>{
-        btn.classList.remove("active")
-    });
+// const btns = document.querySelectorAll(".nav-btn");
+// const slides = document.querySelectorAll(".img-slide");
+// const contents = document.querySelectorAll(".content");
 
-    slides.forEach((slide)=>{
-        slide.classList.remove("active")
-    });
+// var sliderNav = function (manuaL){
+//     btns.forEach((btn)=>{
+//         btn.classList.remove("active")
+//     });
 
-    contents.forEach((content)=>{
-        content.classList.remove("active")
-    });
+//     slides.forEach((slide)=>{
+//         slide.classList.remove("active")
+//     });
 
-    btns[manuaL].classList.add("active")
-    slides[manuaL].classList.add("active")
-    contents[manuaL].classList.add("active")
+//     contents.forEach((content)=>{
+//         content.classList.remove("active")
+//     });
 
-    btns.forEach((btn, i)=>{
-        btn.addEventListener("click", ()=>{
-            sliderNav(i)
-        })
+//     btns[manuaL].classList.add("active")
+//     slides[manuaL].classList.add("active")
+//     contents[manuaL].classList.add("active")
+
+//     btns.forEach((btn, i)=>{
+//         btn.addEventListener("click", ()=>{
+//             sliderNav(i)
+//         })
+//     })
+// }
+
+
+// second
+
+// let next = document.querySelector(".next")
+// let prev = document.querySelector(".prev")
+
+// let slider = document.querySelector(".slider");
+
+// next.addEventListener("click", ()=>{
+//     let slides = document.querySelectorAll(".slides");
+//     slider.appendChild(slides[0])
+//     console.log(slider);
+// })
+
+// prev.addEventListener("click", ()=>{
+//     let slides = document.querySelectorAll(".slides");
+//     slider.prepend(slides[slides.length - 1])
+//     console.log(slider);
+
+// })
+
+
+
+// const slider = document.querySelector(".slider");
+// const buttons = document.querySelectorAll(".buttons span");
+
+
+
+// buttons.forEach((btn)=>{
+//     btn.addEventListener("click", ()=>{
+//         const slides = document.querySelectorAll(".slides");
+//         if(btn.className === "next"){
+//             slider.appendChild(slides[0])
+//         } else{
+//             slider.prepend(slides[slides.length-1])
+//         }
+//     })
+// })
+
+
+// third
+
+const buttons = document.querySelectorAll(".buttons div");
+const slider = document.querySelector(".slider");
+
+buttons.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+
+       const slide = document.querySelectorAll(".slide");
+
+        if(btn.className === "next"){
+            slider.appendChild(slide[0])
+        } else{
+            slider.prepend(slide[slide.length - 1])
+        }
     })
-}
+})
