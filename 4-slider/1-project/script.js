f7d3842bd896c3db1e6673bea33416a4c4fc3140
Mustafizur <mustafizurrahman1256@gmail.com>
2024-09-11 12:40:28 +0600

    draggable-slide-added

--- 4-slider/1-project/script.js
+++ 4-slider/1-project/script.js
@@ -1,171 +1,4 @@
-
-
-// real 
-
-const warpper = document.querySelector(".wrapper");
-
-const carousel = document.querySelector(".carousel");
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-
-const carouselChildrens = [...carousel.children];
-console.log(carouselChildrens);
-let cardPerview = Math.round(carousel.offsetWidth / firstCardWidth);
-
-
-
-carouselChildrens.slice(cardPerview).reverse().forEach(card => {
-    console.log(card);
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-
-carouselChildrens.slice(0,cardPerview).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
-
-console.log(carousel.scrollLeft);
-
-let isDragging = false, prevPageX, prevScrollLeft, timeoutId;
-
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", (e)=>{
-        let id = e.target.id;
-        if(id === "left"){
-            carousel.scrollLeft = carousel.scrollLeft + -firstCardWidth;
-        } else{
-            carousel.scrollLeft =  carousel.scrollLeft + firstCardWidth;
-        }
-    })
-});
-
-const dragStart = (e) =>{
-    isDragging = true;
-    carousel.classList.add("dragging")
-
-    prevPageX = e.pageX;
-    prevScrollLeft = carousel.scrollLeft;
-   
-}
-
-
-
-const dragging = (e) =>{
-    if(!isDragging) return;
-    let positionDiff = e.pageX - prevPageX;
-    console.log(prevPageX);
-    console.log(e.pageX);
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
-
-const dragStop = () =>{
-    isDragging = false;
-    carousel.classList.remove("dragging")
-}
-
-const autoPlay = () =>{
-    if(window.innerWidth < 800)return;
-    timeoutId = setTimeout(()=> carousel.scrollLeft += firstCardWidth, 1000);
-}
-
-autoPlay();
-
-const infifinityScroll = () => {
-    if (carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition")
-      carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-      carousel.classList.remove("no-transition")
-    } else if (
-      Math.ceil(carousel.scrollLeft) ===
-      carousel.scrollWidth - carousel.offsetWidth
-    ) {
-        carousel.classList.add("no-transition")
-      carousel.scrollLeft = carousel.offsetWidth;
-      carousel.classList.remove("no-transition")
-    }
-
-    clearTimeout(timeoutId)
-    if(!warpper.matches(":hover")) autoPlay();
-  };
-
-
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("scroll", infifinityScroll);
-warpper.addEventListener("mouseenter",()=> clearTimeout(timeoutId));
-warpper.addEventListener("mouseleave", autoPlay);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const carousel = document.querySelector(".carousel");
-
-
-// let isDragging = false, startX, startScrollLeft;
-
-// const dragStart = (e) =>{
-//     isDragging = true;
-//     carousel.classList.add("dragging")
-
-//     startX = e.pageX;
-//     startScrollLeft = carousel.scrollLeft;
-// }
-
-// const dragging = (e) =>{
-//     if(!isDragging) return;
-//     carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
-// }
-
-// const dragStop = () =>{
-//     isDragging = false;
-//     carousel.classList.remove("dragging")
-// }
-
-// carousel.addEventListener("mousemove", dragging);
-// carousel.addEventListener("mousedown", dragStart);
-// carousel.addEventListener("mouseup", dragStop);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// third.......
-
+// // real
 
 // const warpper = document.querySelector(".wrapper");
 
@@ -173,109 +6,144 @@ warpper.addEventListener("mouseleave", autoPlay);
 // const arrowBtns = document.querySelectorAll(".wrapper i");
 // const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 
+// const carouselChildrens = [...carousel.children];
 
+// let cardPerview = Math.round(carousel.offsetWidth / firstCardWidth);
 
+// // carouselChildrens.slice(cardPerview).reverse().forEach(card => {
+// //     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+// // });
 
-
-// let isDragging = false, prevPageX, prevScrollLeft, timeoutId;
-
-// arrowBtns.forEach(btn => {
-//     btn.addEventListener("click", (e)=>{
-//         let id = e.target.id;
-//         if(id === "left"){
-//             carousel.scrollLeft = carousel.scrollLeft + -firstCardWidth;
-//         } else{
-//             carousel.scrollLeft =  carousel.scrollLeft + firstCardWidth;
-//         }
-//     })
+// carouselChildrens.slice(0, cardPerview).forEach((card) => {
+//   carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 // });
 
-// const dragStart = (e) =>{
-//     isDragging = true;
-//     carousel.classList.add("dragging")
+// let isDragging = false,
+//   prevPageX,
+//   prevScrollLeft,
+//   timeoutId;
 
-//     prevPageX = e.pageX;
-//     prevScrollLeft = carousel.scrollLeft;
-   
-// }
+// arrowBtns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     let id = e.target.id;
+//     if (id === "left") {
+//       carousel.scrollLeft = carousel.scrollLeft + -firstCardWidth;
+//     } else {
+//       carousel.scrollLeft = carousel.scrollLeft + firstCardWidth;
+//       console.log(carousel.scrollLeft);
+//     }
+//   });
+// });
 
+// const dragStart = (e) => {
+//   isDragging = true;
+//   carousel.classList.add("dragging");
 
+//   prevPageX = e.pageX;
+//   prevScrollLeft = carousel.scrollLeft;
+// };
 
-// const dragging = (e) =>{
-//     if(!isDragging) return;
-//     let positionDiff = e.pageX - prevPageX;
-//     console.log(prevPageX);
-//     console.log(e.pageX);
-//     carousel.scrollLeft = prevScrollLeft - positionDiff;
-// }
+// const dragging = (e) => {
+//   if (!isDragging) return;
+//   let positionDiff = e.pageX - prevPageX;
+//   carousel.scrollLeft = prevScrollLeft - positionDiff;
+// };
 
-// const dragStop = () =>{
-//     isDragging = false;
-//     carousel.classList.remove("dragging")
-// }
+// const dragStop = () => {
+//   isDragging = false;
+//   carousel.classList.remove("dragging");
+// };
 
+// const autoPlay = () => {
+//   if (window.innerWidth < 800) return;
+//   timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 1000);
+// };
 
+// autoPlay();
+
+// const infifinityScroll = () => {
+//   if (carousel.scrollLeft === 0) {
+//     carousel.classList.add("no-transition");
+//     carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+
+//     carousel.classList.remove("no-transition");
+//   } else if (
+//     Math.ceil(carousel.scrollLeft) ===
+//     carousel.scrollWidth - carousel.offsetWidth
+//   ) {
+//     carousel.classList.add("no-transition");
+//     // console.log(carousel.offsetWidth);
+//     carousel.scrollLeft = 0;
+//     console.log(carousel.scrollLeft);
+//     carousel.classList.remove("no-transition");
+//   }
+
+//   clearTimeout(timeoutId);
+//   if (!warpper.matches(":hover")) autoPlay();
+// };
 
 // carousel.addEventListener("mousemove", dragging);
 // carousel.addEventListener("mousedown", dragStart);
 // carousel.addEventListener("mouseup", dragStop);
+// carousel.addEventListener("scroll", infifinityScroll);
+// warpper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+// warpper.addEventListener("mouseleave", autoPlay);
 
 
 
 
 
-
-// const warpper = document.querySelector(".wrapper");
+// // fake
 
 // const carousel = document.querySelector(".carousel");
 // const arrowBtns = document.querySelectorAll(".wrapper i");
-// const cardWidth = carousel.querySelector(".card").offsetWidth;
+// const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 
+// let startX = "";
+// let mousedownClicked = false;
+// let previousScrollLeft = "";
+// let counter = 0;
 
-
-
-
-// let isDragging = false, startX, startScrollLeft;
-
-
-
-// arrowBtns.forEach(btn => {
-//     btn.addEventListener("click", (e)=>{
-//         let id = e.target.id;
-//         if(id === "left"){
-//             carousel.scrollLeft = carousel.scrollLeft + -cardWidth;
+// arrowBtns.forEach((btn)=>{
+//     btn.addEventListener("click", ()=>{
+//         if(btn.id === "right"){
+//             counter ++;
+//             carousel.scrollLeft = firstCardWidth * counter;
+//             if(counter > 4){
+//                 counter = 0;
+//                 carousel.scrollLeft = firstCardWidth * counter;
+//             }
 //         } else{
-//             carousel.scrollLeft =  carousel.scrollLeft + cardWidth;
+//             counter --
+//             if(counter < 0){
+//                 counter = 0
+//                 carousel.scrollLeft = firstCardWidth * counter;
+//             }
 //         }
 //     })
-// });
+// })
 
 
-// let dragStart = (e) =>{
-//     isDragging = true;
-//     carousel.classList.add("dragging")
 
+
+// carousel.addEventListener("mousedown", (e)=>{
+//     mousedownClicked = true
 //     startX = e.pageX;
-//     startScrollLeft = carousel.scrollLeft;
-// }
+//     carousel.classList.add("dragging")
+//     previousScrollLeft = carousel.scrollLeft
+// })
 
+// carousel.addEventListener("mousemove", (e)=>{
+//     if(mousedownClicked){
+//         let totalMouseMove = e.pageX - startX;
 
-// let dragging = (e) =>{
-//     if(!isDragging) return;
-//     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-// }
+//         carousel.scrollLeft = previousScrollLeft - totalMouseMove;
+//     }
+// })
 
-// let dragStop = () =>{
-//     isDragging = false;
-
+// carousel.addEventListener("mouseup", ()=>{
 //     carousel.classList.remove("dragging")
-// }
-
-// carousel.addEventListener("mousemove", dragging);
-// carousel.addEventListener("mousedown", dragStart);
-// carousel.addEventListener("mouseup", dragStop);
-
-
-
+//     mousedownClicked = false;
+// })
 
 
