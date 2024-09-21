9bed4345914baac35d12035104f4dcd300c13963
Mustafizur <mustafizurrahman1256@gmail.com>
2024-09-22 00:43:21 +0600

    slide-edit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const items = document.querySelectorAll(".item");
 console.log(items.length);
 
-const number = document.querySelector(".number").innerHTML= items.length;
+const number = (document.querySelector(".number").innerHTML = items.length);
