5be2177c1eb86f609cf0f80b3003e6b11c14ac05
Mustafizur <mustafizurrahman1256@gmail.com>
2024-09-21 21:53:06 +0600

    slide-project-added

---
+++ 4-slider/1-project/script.js
@@ -0,0 +1,71 @@
+const wrapper = document.querySelector(".wrapper");
+const slides = document.querySelector(".slides");
+const slideAll = document.querySelectorAll(".slide");
+const slideWidth = 366
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+
+let count = 0;
+
+
+    const slidePosition = () =>{
+         slides.style.transform = `translateX(${-slideWidth * count}px)`
+         slides.style.transition = ".5s"
+    }
+
+    slidePosition()
+
+    const increment = () =>{
+        count ++
+        slidePosition()
+    }
+
+    const decrement = () =>{
+        count --;
+        slidePosition()
+    }
+
+    const destinationEnd = () =>{
+        slides.addEventListener("transitionend", ()=>{
+            if(count > 6){
+                count = 0;
+                slides.style.transform = `translateX(${-slideWidth * count}px)`
+                slides.style.transition = "none"
+            }
+        })
+    }
+
+next.addEventListener("click", ()=>{
+    increment()
+    destinationEnd();
+})
+
+prev.addEventListener("click", ()=>{
+    decrement()
+
+    slides.addEventListener("transitionend", ()=>{
+        if(count < 0){
+            count = 4;
+            slides.style.transform = `translateX(${-slideWidth * count}px)`
+            slides.style.transition = "none"
+        }
+    })
+
+})
+
+const timing = () =>{
+   timeInterval = setInterval(()=>{
+       increment()
+       destinationEnd();
+    },2000)
+}
+
+timing()
+
+wrapper.addEventListener("mouseenter", ()=>{
+    clearInterval(timeInterval)
+})
+
+wrapper.addEventListener("mouseleave", ()=>{
+    timing();
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+const items = document.querySelectorAll(".item");
+console.log(items.length);
+
+const number = document.querySelector(".number").innerHTML= items.length;
